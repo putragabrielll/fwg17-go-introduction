0c376b0af3094271c5ececc78bb65d14a906ffc1
putragabrielll <putragmahk@gmail.com>
2024-01-22 17:06:50 +0700

    fix: segitiga terbalik

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("print line from main!")
-
 	printSegitiga(5)
 }
--- task1.go
+++ task1.go
@@ -2,18 +2,34 @@ package main
 
 import "fmt"
 
-func printSegitiga(angka int) {
-	// var k int
-	for initial := 1; angka >= initial; angka-- {
-		// k = angka
-		// angka = 5
-		for i := 1; i <= angka; i++ {
-			fmt.Print(angka)
+func printSegitiga(angka int) { // 5
+	var p int = 1
+	for initial := 1; initial <= angka; initial++ {
+		if initial != 1 {
+			// cara 1
+			// for i := 1; i <= angka-(angka - initial + 1); i++ {
+			// 	fmt.Print("  ")
+			// }
+
+			// cara 2
+			for i := 1; i <= p; i++ {
+				fmt.Print("  ")
+			}
+			p++
 		}
 
-		for j := 1; j <= angka - initial; j++ {
-			fmt.Print(" ")
+		// var h int = angka * 2 - 1
+		// if h == angka {
+		// 	fmt.Print(("  "))
+		// }
+
+		for i := 1; i <= angka - initial + 1; i++ {
+			fmt.Print("* ")	
 		}
+		for j := 1; j <= angka - initial; j++ {
+			fmt.Print("* ")
+		}
+		
 
 		// for {
 		// 	fmt.Print("* ")
