f0d43e210f4e411ff315803f70cd5cddc4f4fc86
putragabrielll <putragmahk@gmail.com>
2024-01-26 11:15:27 +0700

    fix: task 2

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-// import "fmt"
 
 func main() {
-	// printSegitiga(5)
-	// fmt.Println(MainData(7)) // 7 8 9 10 11 12
-	fmt.Println(GenPass("abcdefg", "medium"))
+	fmt.Println("Welcome To My New Website!")
+	// printSegitiga(7)
+	// fmt.Println(MainData(12)) // 7 8 9 10 11 12
+	// fmt.Println(GenPass("abcdefg", "strong"))
 }
--- task2.go
+++ task2.go
@@ -13,8 +13,8 @@ func proccess(teks string, characters string, angka string) string{ // logic pas
 	output := []string{"", "", "", "", "", "", ""}
 
 	for i, v := range data1 {
-		random := rand.Float32()
-		bulat := math.Round(float64(random))
+		random := rand.Float32()  // 0.3412831238
+		bulat := math.Round(float64(random)) // 1 atau 0
 		// fmt.Println(bulat)
 		if bulat == 1 {
 			temp := strings.ToUpper(v)
@@ -24,6 +24,7 @@ func proccess(teks string, characters string, angka string) string{ // logic pas
 			output[i] = temp
 		}
 	}
+	// AbC
 	
 	gabung := strings.Join(output, "")
 	return (gabung + characters + angka) // output
@@ -35,21 +36,21 @@ func GenPass(pass string, level string) string{
 	} else if len(pass) > 7 {
 		return "Password Over load...\nPassword must be 6 - 7 characters !"
 	}
-	random := rand.Perm(9) // mencari random angka
+	random := rand.Perm(9) // mencari random angka [1 2 3 4 5 9]
 	angka := 3
 	if level == "medium" {
 		angka = 4
 	} else if level == "strong" {
 		angka = 5
 	}
-	hasilAngka := random[0:angka]
-	outputAngka := strings.Trim(strings.Replace(fmt.Sprint(hasilAngka), " ", "", -1), "[]")
+	hasilAngka := random[0:angka] // [3 4 2]
+	outputAngka := strings.Trim(strings.Replace(fmt.Sprint(hasilAngka), " ", "", -1), "[]") // 342
 
 	specialCharacters := "@:;#&-?/%+*" // mencari random character
-	special := strings.Split(specialCharacters, "") // [@ : ; # & - ? / % + *]
+	special := strings.Split(specialCharacters, "") // [@ : ; # & - ? / % + *] 11
 	panjang := len(special)
-	getspesial := rand.Intn(panjang)
-	char := ""
+	getspesial := rand.Intn(panjang) // 2
+	char := "" // ;
 
 	for i, v := range special{
 		if i == getspesial {
