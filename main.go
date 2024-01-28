d2c62e7179c3238f17d25c060e95ab073bfdb28c
putragabrielll <putragmahk@gmail.com>
2024-01-28 13:24:22 +0700

    fix: commant

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome To My New Website!")
+
+	// Task 1
 	// printSegitiga(7)
+
+	// Task 2
 	// fmt.Println(MainData(12)) // 7 8 9 10 11 12
+
+	// Task 3
 	// fmt.Println(GenPass("abcdefg", "strong"))
 }
